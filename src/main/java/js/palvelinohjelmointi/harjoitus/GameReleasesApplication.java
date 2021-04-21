07203624ff99234cb7748bbca4bbcb96f739b7bd
Joonas Saarela <jsaarela98@gmail.com>
2021-04-21 12:19:43 +0300

    project initialized

---
+++ src/main/java/js/palvelinohjelmointi/harjoitus/GameReleasesApplication.java
@@ -0,0 +1,13 @@
+package js.palvelinohjelmointi.harjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReleasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameReleasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/js/palvelinohjelmointi/harjoitus/GameReleasesApplicationTests.java
@@ -0,0 +1,13 @@
+package js.palvelinohjelmointi.harjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReleasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
